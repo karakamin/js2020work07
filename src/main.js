d8811af4610359ff7dea3f88634a9633f5b5577b
karakamin <geminicolor@gmail.com>
2020-08-17 11:45:38 +0800

    add work 07

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
+// Import component
+import Loading from 'vue-loading-overlay';
+// Import stylesheet
+import 'vue-loading-overlay/dist/vue-loading.css';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.component('Loading', Loading);
 
 new Vue({
   router,
